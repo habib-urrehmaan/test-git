4c8349937a736fa98e18d0a87c4c2335d8c98703
Habib Ur Rehman <habiburrehman012@gmail.com>
2021-01-30 12:53:07 +0500

    Initialized git repository

---
+++ hello.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    document.getElementById('desc').style.color = "green";
+    clickHandler = function () {
+        document.getElementsByClassName('desc').style.color = "green";
+    }
+}
